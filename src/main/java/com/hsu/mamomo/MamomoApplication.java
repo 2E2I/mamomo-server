a3d627d9c57573473bd21863442842715815083d
임유선 <1991167@hansung.ac.kr>
2022-02-19 00:03:33 +0900

    :sparkles: create project

---
+++ src/main/java/com/hsu/mamomo/MamomoApplication.java
@@ -0,0 +1,13 @@
+package com.hsu.mamomo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MamomoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MamomoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hsu/mamomo/MamomoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsu.mamomo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MamomoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
