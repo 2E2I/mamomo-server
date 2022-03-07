51c87d91a9dd60f055d6f02bfb272c0bedf657cd
임유선 <1991167@hansung.ac.kr>
2022-03-07 23:33:21 +0900

    :bug: es 컬럼과 매칭

--- src/main/java/com/hsu/mamomo/domain/Campaign.java
+++ src/main/java/com/hsu/mamomo/domain/Campaign.java
@@ -1,12 +1,13 @@
 package com.hsu.mamomo.domain;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+import org.springframework.lang.Nullable;
 
 import javax.persistence.*;
 import java.util.List;
@@ -17,24 +18,47 @@ import java.util.List;
 @Document(indexName = "campaigns")
 @Entity
 public class Campaign {
+
+    @Nullable
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
     private String id;
+
+    @Field(name = "site_type", type = FieldType.Keyword)
     private String siteType;
+
+    @Field(name = "url", type = FieldType.Keyword)
     private String url;
+
+    @Field(name = "title", type = FieldType.Keyword)
     private String title;
+
     @ElementCollection
+    @Field(name = "tags", type = FieldType.Keyword)
     private List<String> tags;
+
+    @Field(name = "body", type = FieldType.Keyword)
     private String body;
+
+    @Field(name = "organization_name", type = FieldType.Keyword)
     private String organizationName;
+
+    @Field(name = "thumbnail", type = FieldType.Keyword)
     private String thumbnail;
 
     // 편의상 String으로 설정
-    // 자료형 후에 반드시 변경할것
+    @Field(name = "due_date", type = FieldType.Keyword)
     private String dueDate;
+
+    @Field(name = "start_date", type = FieldType.Keyword)
     private String startDate;
+
+    @Field(name = "target_price", type = FieldType.Keyword)
     private String targetPrice;
+
+    @Field(name = "status_price", type = FieldType.Keyword)
     private String statusPrice;
+
+    @Field(name = "percent", type = FieldType.Keyword)
     private String percent;
 
 }
