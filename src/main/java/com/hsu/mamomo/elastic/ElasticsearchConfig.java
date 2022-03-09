b60110e587b42175b2cd65feb9903b7200763dd3
임유선 <1991167@hansung.ac.kr>
2022-03-10 00:22:03 +0900

    :poop: 실수 원복

--- src/main/java/com/hsu/mamomo/elastic/ElasticsearchConfig.java
+++ src/main/java/com/hsu/mamomo/elastic/ElasticsearchConfig.java
@@ -30,8 +30,8 @@ public class ElasticsearchConfig extends AbstractElasticsearchConfiguration {
     public RestHighLevelClient elasticsearchClient() {
 
         final ClientConfiguration clientConfiguration = ClientConfiguration.builder()
-                .connectedTo("localhost:9200")
-                //.withBasicAuth(username, password)
+                .connectedTo(host + ":" + port)
+                .withBasicAuth(username, password)
                 .build();
 
         return RestClients.create(clientConfiguration).rest();
