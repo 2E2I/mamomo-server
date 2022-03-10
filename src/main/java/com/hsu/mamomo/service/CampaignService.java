7cd9ea16b1627591488d19dc2eb966754e474b3f
im-shung <seoyoung1113@gmail.com>
2022-03-10 21:14:49 +0900

    :sparkles: 캠페인 전체 보기 api 구현 (#8) /campaigns == /campaigns?sort=1 - SortFactory 클래스로 팩토리 메서드 구í구현해봄

--- src/main/java/com/hsu/mamomo/service/CampaignService.java
+++ src/main/java/com/hsu/mamomo/service/CampaignService.java
@@ -2,20 +2,75 @@ package com.hsu.mamomo.service;
 
 import com.hsu.mamomo.domain.Campaign;
 import com.hsu.mamomo.repository.CampaignRepository;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.elasticsearch.search.sort.SortBuilders;
+import org.elasticsearch.search.sort.SortOrder;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.SearchHits;
+import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
+import org.springframework.data.elasticsearch.core.query.Query;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class CampaignService {
-
     private final CampaignRepository campaignRepository;
+    private final ElasticsearchOperations elasticsearchOperations;
+    private final SortFactory sortFactory;
+    private static final String CAMPAIGN_INDEX = "campaigns";
 
-    public List<Campaign> findAll() {
-        return campaignRepository.findAll();
+
+    /*
+     * 캠페인 전체 보기 - 최신 순
+     * */
+    public List<Campaign> findAllByStartDate() {
+        // 1. Create sort Query
+        Query sortQuery = sortFactory.createSortQuery("StartDate");
+
+        // 2. Execute search
+        SearchHits<Campaign> campaignHits =
+                elasticsearchOperations.search(sortQuery,
+                        Campaign.class,
+                        IndexCoordinates.of(CAMPAIGN_INDEX)
+                );
+
+        // 3. Map searchHits to Campaign list
+        List<Campaign> campaignMatches = new ArrayList<Campaign>();
+        campaignHits.forEach(
+                searchHit -> {
+                    campaignMatches.add(searchHit.getContent());
+                });
+
+        return campaignMatches;
     }
+
+
+    /*
+     * 캠페인 전체 보기 - 종료 임박 순
+     * */
+    public List<Campaign> findAllByDueDate() {
+        // 1. Create sort Query
+        Query sortQuery = sortFactory.createSortQuery("DueDate");
+
+        // 2. Execute search
+        SearchHits<Campaign> campaignHits =
+                elasticsearchOperations.search(sortQuery,
+                        Campaign.class,
+                        IndexCoordinates.of(CAMPAIGN_INDEX)
+                );
+
+        // 3. Map searchHits to Campaign list
+        List<Campaign> campaignMatches = new ArrayList<Campaign>();
+        campaignHits.forEach(
+                searchHit -> {
+                    campaignMatches.add(searchHit.getContent());
+                });
+
+        return campaignMatches;
+    }
+
 }
