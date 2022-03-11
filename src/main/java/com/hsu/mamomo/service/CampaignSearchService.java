2361c3987566751b933dda9443f27dffa5fdbf9d
im-shung <seoyoung1113@gmail.com>
2022-03-11 14:47:47 +0900

    :art: ES 검색 로직을 추상 팩토리 패턴에 맞춰 수정 (#18)

--- src/main/java/com/hsu/mamomo/service/CampaignSearchService.java
+++ src/main/java/com/hsu/mamomo/service/CampaignSearchService.java
@@ -4,55 +4,40 @@ import com.hsu.mamomo.domain.Campaign;
 import com.hsu.mamomo.repository.CampaignSearchRepository;
 import lombok.RequiredArgsConstructor;
 import org.elasticsearch.index.query.MultiMatchQueryBuilder;
-import org.elasticsearch.index.query.Operator;
-import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.sort.FieldSortBuilder;
-import org.elasticsearch.search.sort.SortBuilders;
-import org.elasticsearch.search.sort.SortOrder;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
-import org.springframework.data.elasticsearch.core.SearchHit;
 import org.springframework.data.elasticsearch.core.SearchHits;
-import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
 import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
 import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
 public class CampaignSearchService {
-    @Autowired
-    private ElasticsearchOperations elasticsearchOperations;
+
     private final CampaignSearchRepository campaignSearchRepository;
-    private static final Map<String, SortOrder> sortMap = createSortMap();
-    private static Map<String, SortOrder> createSortMap() {
-        return Map.of("asc", SortOrder.ASC, "desc", SortOrder.DESC);
-    }
+    private final ElasticSearchFactory factory;
 
     /*
-    * 제목 + 본문 검색 (OR)
-    * */
+     * 제목 + 본문 검색 (OR)
+     * */
     public List<Campaign> searchByTitleOrBody(String keyword, String item, String direction) {
 
+        // 1. Setting up Builder
+        MultiMatchQueryBuilder multiMatchQueryBuilder = factory.createQueryBuilder(keyword);
+        FieldSortBuilder sortBuilder = factory.createSortBuilder(item, direction);
 
-        MultiMatchQueryBuilder multiMatchQueryBuilder = QueryBuilders.multiMatchQuery(keyword,"title", "body")
-                .operator(Operator.AND);
-
-        FieldSortBuilder sortBuilder = SortBuilders.fieldSort(item).order(sortMap.get(direction));
-
-        NativeSearchQuery searchQuery = new NativeSearchQueryBuilder()
+        // 2. Create Query
+        NativeSearchQuery query = new NativeSearchQueryBuilder()
                 .withQuery(multiMatchQueryBuilder)
                 .withSorts(sortBuilder)
                 .build();
 
-        SearchHits<Campaign> searchHit = elasticsearchOperations.search(searchQuery, Campaign.class, IndexCoordinates.of("campaigns"));
-        System.out.println(searchHit);
-        return searchHit.stream()
-                .map(SearchHit::getContent)
-                .collect(Collectors.toList());
+        // 3. Execute search
+        SearchHits<Campaign> searchHits = factory.getSearchHits(query);
+
+        // 4. Map SearchHits to Campaign list
+        return factory.getCampaignList(searchHits);
     }
 }
--- src/main/java/com/hsu/mamomo/service/CampaignService.java
+++ src/main/java/com/hsu/mamomo/service/CampaignService.java
@@ -3,74 +3,40 @@ package com.hsu.mamomo.service;
 import com.hsu.mamomo.domain.Campaign;
 import com.hsu.mamomo.repository.CampaignRepository;
 import lombok.RequiredArgsConstructor;
-import org.elasticsearch.search.sort.SortBuilders;
-import org.elasticsearch.search.sort.SortOrder;
-import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.elasticsearch.search.sort.FieldSortBuilder;
 import org.springframework.data.elasticsearch.core.SearchHits;
-import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
 import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
-import org.springframework.data.elasticsearch.core.query.Query;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class CampaignService {
+
     private final CampaignRepository campaignRepository;
-    private final ElasticsearchOperations elasticsearchOperations;
-    private final SortFactory sortFactory;
-    private static final String CAMPAIGN_INDEX = "campaigns";
-
+    private final ElasticSortFactory factory;
 
     /*
-     * 캠페인 전체 보기 - 최신 순
+     * 캠페인 전체 보기
      * */
-    public List<Campaign> findAllByStartDate() {
-        // 1. Create sort Query
-        Query sortQuery = sortFactory.createSortQuery("StartDate");
+    public List<Campaign> findAll(String item, String direction) {
 
-        // 2. Execute search
-        SearchHits<Campaign> campaignHits =
-                elasticsearchOperations.search(sortQuery,
-                        Campaign.class,
-                        IndexCoordinates.of(CAMPAIGN_INDEX)
-                );
+        // 1. Setting up Builder
+        FieldSortBuilder sortBuilder = factory.createSortBuilder(item, direction);
 
-        // 3. Map searchHits to Campaign list
-        List<Campaign> campaignMatches = new ArrayList<Campaign>();
-        campaignHits.forEach(
-                searchHit -> {
-                    campaignMatches.add(searchHit.getContent());
-                });
+        // 2. Create Query
+        NativeSearchQuery query = new NativeSearchQueryBuilder()
+                .withSorts(sortBuilder)
+                .build();
 
-        return campaignMatches;
+        // 3. Execute search
+        SearchHits<Campaign> searchHits = factory.getSearchHits(query);
+
+        // 4. Map SearchHits to Campaign list
+        return factory.getCampaignList(searchHits);
     }
 
-
-    /*
-     * 캠페인 전체 보기 - 종료 임박 순
-     * */
-    public List<Campaign> findAllByDueDate() {
-        // 1. Create sort Query
-        Query sortQuery = sortFactory.createSortQuery("DueDate");
-
-        // 2. Execute search
-        SearchHits<Campaign> campaignHits =
-                elasticsearchOperations.search(sortQuery,
-                        Campaign.class,
-                        IndexCoordinates.of(CAMPAIGN_INDEX)
-                );
-
-        // 3. Map searchHits to Campaign list
-        List<Campaign> campaignMatches = new ArrayList<Campaign>();
-        campaignHits.forEach(
-                searchHit -> {
-                    campaignMatches.add(searchHit.getContent());
-                });
-
-        return campaignMatches;
-    }
 
 }
