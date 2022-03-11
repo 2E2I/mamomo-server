26bd1e8c21a13df0f949c0953f7192165d1cdf44
임유선 <1991167@hansung.ac.kr>
2022-03-12 00:08:22 +0900

    :white_check_mark: /search 테스트 코드 작성, spring rest docs api.html 적용

--- src/main/java/com/hsu/mamomo/controller/CampaignSearchController.java
+++ src/main/java/com/hsu/mamomo/controller/CampaignSearchController.java
@@ -5,6 +5,7 @@ import com.hsu.mamomo.repository.CampaignSearchRepository;
 import com.hsu.mamomo.service.CampaignSearchService;
 import java.util.HashMap;
 import java.util.Map;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +20,6 @@ import java.util.List;
 public class CampaignSearchController {
 
     private final CampaignSearchService campaignSearchService;
-    private final CampaignSearchRepository campaignSearchRepository;
 
     @GetMapping("/search")
     public Map<String, List<String>> searchTag() {
