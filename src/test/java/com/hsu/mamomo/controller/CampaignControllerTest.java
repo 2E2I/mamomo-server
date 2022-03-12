455245b29c94301056ddc829613690fbafe12da9
im-shung <seoyoung1113@gmail.com>
2022-03-12 22:33:02 +0900

    :ok_hand: /campaigns 관련 테스트 검증 과정 추가 (#9) - 캠페인 각 요소들 타입 검사 campaigns가 배열인지 검사

--- src/test/java/com/hsu/mamomo/controller/CampaignControllerTest.java
+++ src/test/java/com/hsu/mamomo/controller/CampaignControllerTest.java
@@ -1,14 +1,17 @@
 package com.hsu.mamomo.controller;
 
 
-import static com.hsu.mamomo.controller.ApiDocumentUtils.getDocumentRequest;
-import static com.hsu.mamomo.controller.ApiDocumentUtils.getDocumentResponse;
+
+import static com.hsu.mamomo.document.ApiDocumentUtils.getDocumentRequest;
+import static com.hsu.mamomo.document.ApiDocumentUtils.getDocumentResponse;
+import static com.hsu.mamomo.document.DocumentFormatGenerator.getSortFormat;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.relaxedResponseFields;
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
@@ -18,6 +21,7 @@ import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDoc
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -35,23 +39,52 @@ class CampaignControllerTest {
         mockMvc.perform(get("/api/campaigns")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
+                .andExpect(jsonPath("$.campaigns").isArray())
+                .andExpect(jsonPath("$.campaigns.[0].['id']").isString())
+                .andExpect(jsonPath("$.campaigns.[0].['siteType']").isString())
+                .andExpect(jsonPath("$.campaigns.[0].['url']").isString())
+                .andExpect(jsonPath("$.campaigns.[0].['title']").isString())
+                .andExpect(jsonPath("$.campaigns.[0].['tags']").isArray())
+                .andExpect(jsonPath("$.campaigns.[0].['body']").isString())
+                .andExpect(jsonPath("$.campaigns.[0].['organizationName']").isString())
+                .andExpect(jsonPath("$.campaigns.[0].['thumbnail']").isString())
+                .andExpect(jsonPath("$.campaigns.[0].['dueDate']").isString())
+                .andExpect(jsonPath("$.campaigns.[0].['startDate']").isString())
+                .andExpect(jsonPath("$.campaigns.[0].['targetPrice']").isNumber())
+                .andExpect(jsonPath("$.campaigns.[0].['statusPrice']").isNumber())
+                .andExpect(jsonPath("$.campaigns.[0].['percent']").isNumber())
                 .andDo(document("campaigns-default",
                         getDocumentRequest(),
                         getDocumentResponse(),
                         relaxedResponseFields(
-                                fieldWithPath("campaigns.[].id").description("캠페인 아이디"),
-                                fieldWithPath("campaigns.[].siteType").description("기부 사이트 종류"),
-                                fieldWithPath("campaigns.[].url").description("URL 주소"),
-                                fieldWithPath("campaigns.[].title").description("제목"),
-                                fieldWithPath("campaigns.[].tags").description("태그"),
-                                fieldWithPath("campaigns.[].body").description("본문"),
-                                fieldWithPath("campaigns.[].organizationName").description("조직 기관"),
-                                fieldWithPath("campaigns.[].thumbnail").description("썸네일"),
-                                fieldWithPath("campaigns.[].dueDate").description("마감 날짜"),
-                                fieldWithPath("campaigns.[].startDate").description("시작 날짜"),
-                                fieldWithPath("campaigns.[].targetPrice").description("목표 금액"),
-                                fieldWithPath("campaigns.[].statusPrice").description("현재 금액"),
-                                fieldWithPath("campaigns.[].percent").description("달성 정도")
+                                fieldWithPath("campaigns").type(JsonFieldType.ARRAY)
+                                        .description("캠페인 리스트"),
+                                fieldWithPath("campaigns.[].id").type(JsonFieldType.STRING)
+                                        .description("캠페인 아이디"),
+                                fieldWithPath("campaigns.[].siteType").type(JsonFieldType.STRING)
+                                        .description("사이트 타입"),
+                                fieldWithPath("campaigns.[].url").type(JsonFieldType.STRING)
+                                        .description("URL 주소"),
+                                fieldWithPath("campaigns.[].title").type(JsonFieldType.STRING)
+                                        .description("제목"),
+                                fieldWithPath("campaigns.[].tags").type(JsonFieldType.ARRAY)
+                                        .description("태그 모음"),
+                                fieldWithPath("campaigns.[].body").type(JsonFieldType.STRING)
+                                        .description("본문"),
+                                fieldWithPath("campaigns.[].organizationName").type(
+                                        JsonFieldType.STRING).description("조직 기관"),
+                                fieldWithPath("campaigns.[].thumbnail").type(JsonFieldType.STRING)
+                                        .description("썸네일"),
+                                fieldWithPath("campaigns.[].dueDate").type(JsonFieldType.STRING)
+                                        .description("마감 날짜"),
+                                fieldWithPath("campaigns.[].startDate").type(JsonFieldType.STRING)
+                                        .description("시작 날짜"),
+                                fieldWithPath("campaigns.[].targetPrice").type(JsonFieldType.NUMBER)
+                                        .description("목표 금액"),
+                                fieldWithPath("campaigns.[].statusPrice").type(JsonFieldType.NUMBER)
+                                        .description("현재 금액"),
+                                fieldWithPath("campaigns.[].percent").type(JsonFieldType.NUMBER)
+                                        .description("달성 정도")
                         )
                 ));
     }
@@ -62,26 +95,60 @@ class CampaignControllerTest {
                         .content("{\"sort\": \"start_date,desc\"}")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
+                .andExpect(jsonPath("$.campaigns").isArray())
+                .andExpect(jsonPath("$.campaigns.[0].['id']").isString())
+                .andExpect(jsonPath("$.campaigns.[0].['siteType']").isString())
+                .andExpect(jsonPath("$.campaigns.[0].['url']").isString())
+                .andExpect(jsonPath("$.campaigns.[0].['title']").isString())
+                .andExpect(jsonPath("$.campaigns.[0].['tags']").isArray())
+                .andExpect(jsonPath("$.campaigns.[0].['body']").isString())
+                .andExpect(jsonPath("$.campaigns.[0].['organizationName']").isString())
+                .andExpect(jsonPath("$.campaigns.[0].['thumbnail']").isString())
+                .andExpect(jsonPath("$.campaigns.[0].['dueDate']").isString())
+                .andExpect(jsonPath("$.campaigns.[0].['startDate']").isString())
+                .andExpect(jsonPath("$.campaigns.[0].['targetPrice']").isNumber())
+                .andExpect(jsonPath("$.campaigns.[0].['statusPrice']").isNumber())
+                .andExpect(jsonPath("$.campaigns.[0].['percent']").isNumber())
                 .andDo(document("campaigns-latest",
                         getDocumentRequest(),
                         getDocumentResponse(),
                         requestFields(
-                                fieldWithPath("sort").description("정렬 방식 - 최신 순")
+                                fieldWithPath("sort").description(
+                                                "캠페인 리스트 정렬 방식을 결정하는 파라미터, "
+                                                        + "정렬 대상은 'start_date','due_date' 중 하나,"
+                                                        + "정렬 방법은 'asc', 'desc' 중 하나")
+                                        .optional()
+                                        .attributes(getSortFormat())
                         ),
                         relaxedResponseFields(
-                                fieldWithPath("campaigns.[].id").description("캠페인 아이디"),
-                                fieldWithPath("campaigns.[].siteType").description("기부 사이트 종류"),
-                                fieldWithPath("campaigns.[].url").description("URL 주소"),
-                                fieldWithPath("campaigns.[].title").description("제목"),
-                                fieldWithPath("campaigns.[].tags").description("태그"),
-                                fieldWithPath("campaigns.[].body").description("본문"),
-                                fieldWithPath("campaigns.[].organizationName").description("조직 기관"),
-                                fieldWithPath("campaigns.[].thumbnail").description("썸네일"),
-                                fieldWithPath("campaigns.[].dueDate").description("마감 날짜"),
-                                fieldWithPath("campaigns.[].startDate").description("시작 날짜"),
-                                fieldWithPath("campaigns.[].targetPrice").description("목표 금액"),
-                                fieldWithPath("campaigns.[].statusPrice").description("현재 금액"),
-                                fieldWithPath("campaigns.[].percent").description("달성 정도")
+                                fieldWithPath("campaigns").type(JsonFieldType.ARRAY)
+                                        .description("캠페인 리스트"),
+                                fieldWithPath("campaigns.[].id").type(JsonFieldType.STRING)
+                                        .description("캠페인 아이디"),
+                                fieldWithPath("campaigns.[].siteType").type(JsonFieldType.STRING)
+                                        .description("사이트 타입"),
+                                fieldWithPath("campaigns.[].url").type(JsonFieldType.STRING)
+                                        .description("URL 주소"),
+                                fieldWithPath("campaigns.[].title").type(JsonFieldType.STRING)
+                                        .description("제목"),
+                                fieldWithPath("campaigns.[].tags").type(JsonFieldType.ARRAY)
+                                        .description("태그 모음"),
+                                fieldWithPath("campaigns.[].body").type(JsonFieldType.STRING)
+                                        .description("본문"),
+                                fieldWithPath("campaigns.[].organizationName").type(
+                                        JsonFieldType.STRING).description("조직 기관"),
+                                fieldWithPath("campaigns.[].thumbnail").type(JsonFieldType.STRING)
+                                        .description("썸네일"),
+                                fieldWithPath("campaigns.[].dueDate").type(JsonFieldType.STRING)
+                                        .description("마감 날짜"),
+                                fieldWithPath("campaigns.[].startDate").type(JsonFieldType.STRING)
+                                        .description("시작 날짜"),
+                                fieldWithPath("campaigns.[].targetPrice").type(JsonFieldType.NUMBER)
+                                        .description("목표 금액"),
+                                fieldWithPath("campaigns.[].statusPrice").type(JsonFieldType.NUMBER)
+                                        .description("현재 금액"),
+                                fieldWithPath("campaigns.[].percent").type(JsonFieldType.NUMBER)
+                                        .description("달성 정도")
                         )
                 ));
     }
@@ -92,26 +159,60 @@ class CampaignControllerTest {
                         .content("{\"sort\": \"due_date,asc\"}")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
+                .andExpect(jsonPath("$.campaigns").isArray())
+                .andExpect(jsonPath("$.campaigns.[0].['id']").isString())
+                .andExpect(jsonPath("$.campaigns.[0].['siteType']").isString())
+                .andExpect(jsonPath("$.campaigns.[0].['url']").isString())
+                .andExpect(jsonPath("$.campaigns.[0].['title']").isString())
+                .andExpect(jsonPath("$.campaigns.[0].['tags']").isArray())
+                .andExpect(jsonPath("$.campaigns.[0].['body']").isString())
+                .andExpect(jsonPath("$.campaigns.[0].['organizationName']").isString())
+                .andExpect(jsonPath("$.campaigns.[0].['thumbnail']").isString())
+                .andExpect(jsonPath("$.campaigns.[0].['dueDate']").isString())
+                .andExpect(jsonPath("$.campaigns.[0].['startDate']").isString())
+                .andExpect(jsonPath("$.campaigns.[0].['targetPrice']").isNumber())
+                .andExpect(jsonPath("$.campaigns.[0].['statusPrice']").isNumber())
+                .andExpect(jsonPath("$.campaigns.[0].['percent']").isNumber())
                 .andDo(document("campaigns-deadline",
                         getDocumentRequest(),
                         getDocumentResponse(),
                         requestFields(
-                                fieldWithPath("sort").description("정렬 방식 - 마감 순")
+                                fieldWithPath("sort").description(
+                                                "캠페인 리스트 정렬 방식을 결정하는 파라미터, "
+                                                        + "정렬 대상은 'start_date','due_date' 중 하나,"
+                                                        + "정렬 방법은 'asc', 'desc' 중 하나")
+                                        .optional()
+                                        .attributes(getSortFormat())
                         ),
                         relaxedResponseFields(
-                                fieldWithPath("campaigns.[].id").description("캠페인 아이디"),
-                                fieldWithPath("campaigns.[].siteType").description("기부 사이트 종류"),
-                                fieldWithPath("campaigns.[].url").description("URL 주소"),
-                                fieldWithPath("campaigns.[].title").description("제목"),
-                                fieldWithPath("campaigns.[].tags").description("태그"),
-                                fieldWithPath("campaigns.[].body").description("본문"),
-                                fieldWithPath("campaigns.[].organizationName").description("조직 기관"),
-                                fieldWithPath("campaigns.[].thumbnail").description("썸네일"),
-                                fieldWithPath("campaigns.[].dueDate").description("마감 날짜"),
-                                fieldWithPath("campaigns.[].startDate").description("시작 날짜"),
-                                fieldWithPath("campaigns.[].targetPrice").description("목표 금액"),
-                                fieldWithPath("campaigns.[].statusPrice").description("현재 금액"),
-                                fieldWithPath("campaigns.[].percent").description("달성 정도")
+                                fieldWithPath("campaigns").type(JsonFieldType.ARRAY)
+                                        .description("캠페인 리스트"),
+                                fieldWithPath("campaigns.[].id").type(JsonFieldType.STRING)
+                                        .description("캠페인 아이디"),
+                                fieldWithPath("campaigns.[].siteType").type(JsonFieldType.STRING)
+                                        .description("사이트 타입"),
+                                fieldWithPath("campaigns.[].url").type(JsonFieldType.STRING)
+                                        .description("URL 주소"),
+                                fieldWithPath("campaigns.[].title").type(JsonFieldType.STRING)
+                                        .description("제목"),
+                                fieldWithPath("campaigns.[].tags").type(JsonFieldType.ARRAY)
+                                        .description("태그 모음"),
+                                fieldWithPath("campaigns.[].body").type(JsonFieldType.STRING)
+                                        .description("본문"),
+                                fieldWithPath("campaigns.[].organizationName").type(
+                                        JsonFieldType.STRING).description("조직 기관"),
+                                fieldWithPath("campaigns.[].thumbnail").type(JsonFieldType.STRING)
+                                        .description("썸네일"),
+                                fieldWithPath("campaigns.[].dueDate").type(JsonFieldType.STRING)
+                                        .description("마감 날짜"),
+                                fieldWithPath("campaigns.[].startDate").type(JsonFieldType.STRING)
+                                        .description("시작 날짜"),
+                                fieldWithPath("campaigns.[].targetPrice").type(JsonFieldType.NUMBER)
+                                        .description("목표 금액"),
+                                fieldWithPath("campaigns.[].statusPrice").type(JsonFieldType.NUMBER)
+                                        .description("현재 금액"),
+                                fieldWithPath("campaigns.[].percent").type(JsonFieldType.NUMBER)
+                                        .description("달성 정도")
                         )
                 ));
     }
