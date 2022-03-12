4c4a627e8b9d2be730e3c668978330195384c851
im-shung <seoyoung1113@gmail.com>
2022-03-12 22:30:44 +0900

    :art: 추가 입력 포맷 인터페이스 - attributes에 key와 values를 각각 넣을수 있지만 반복적으로 사용되는 것들은 static 메서드로 사용

---
+++ src/test/java/com/hsu/mamomo/document/DocumentFormatGenerator.java
@@ -0,0 +1,14 @@
+package com.hsu.mamomo.document;
+
+import static org.springframework.restdocs.snippet.Attributes.key;
+
+import org.springframework.restdocs.snippet.Attributes;
+
+public interface DocumentFormatGenerator {
+
+    static Attributes.Attribute getSortFormat() {
+        return key("format").value("{정렬대상}, {정렬방법}");
+    }
+
+
+}
