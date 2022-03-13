fbc3b88c7292ffb4eaa8487ee37a115fdb0f4600
im-shung <seoyoung1113@gmail.com>
2022-03-13 21:42:34 +0900

    :sparkles: 카테고리 id -> 카테고리 이름 매칭 로직 구현 (#35)

--- src/main/java/com/hsu/mamomo/service/ElasticCategoryFactory.java
+++ src/main/java/com/hsu/mamomo/service/ElasticCategoryFactory.java
@@ -2,31 +2,48 @@ package com.hsu.mamomo.service;
 
 import com.hsu.mamomo.domain.Campaign;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.sort.SortOrder;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.SearchHit;
 import org.springframework.data.elasticsearch.core.SearchHits;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ElasticCategoryFactory extends ElasticFactory{
+public class ElasticCategoryFactory extends ElasticFactory {
+
+    private static final Map<Integer, String> categoryMap
+            = Map.of(1, "아동|청소년",
+            2, "어르신",
+            3, "장애인",
+            4, "어려운이웃",
+            5, "다문화",
+            6, "지구촌",
+            7, "가족|여성",
+            8, "우리사회",
+            9, "동물",
+            10, "환경");
 
     public ElasticCategoryFactory(
             ElasticsearchOperations elasticsearchOperations) {
         super(elasticsearchOperations);
     }
 
-        @Override
-        public QueryBuilder createQueryBuilder(String keyword) {
-            System.out.println("keyword = " + keyword);
-            return QueryBuilders.matchQuery("category",keyword);
-        }
+    public String matchCategoryNameByCategoryId(int category_id) {
+        return categoryMap.get(category_id);
+    }
 
-        @Override
-        public List<Campaign> getCampaignList(SearchHits<Campaign> searchHits) {
-            return searchHits.stream()
+    @Override
+    public QueryBuilder createQueryBuilder(String keyword) {
+        return QueryBuilders.matchQuery("category",keyword);
+    }
+
+    @Override
+    public List<Campaign> getCampaignList(SearchHits<Campaign> searchHits) {
+        return searchHits.stream()
                 .map(SearchHit::getContent)
                 .collect(Collectors.toList());
     }
