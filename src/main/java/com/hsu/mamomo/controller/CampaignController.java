dc80514fc4e5419a0000865f4e1d9fcb57c61afd
im-shung <seoyoung1113@gmail.com>
2022-03-13 21:39:41 +0900

    :label: category 파라미터 type(String->Integer) 수정 (#35)

--- src/main/java/com/hsu/mamomo/controller/CampaignController.java
+++ src/main/java/com/hsu/mamomo/controller/CampaignController.java
@@ -19,17 +19,16 @@ public class CampaignController {
     @GetMapping("/campaigns")
     public Map<String, List<Campaign>> getAllCampaigns(
             @RequestParam(value = "sort", defaultValue = "start_date,desc", required = false) String sort,
-            @RequestParam(value = "category", required = false) String category) {
+            @RequestParam(value = "category", required = false) Integer category_id) {
 
         Map<String, List<Campaign>> result = new HashMap<>();
 
         String[] _sort = sort.split(",");
         // sort = [field, direction]
 
-        if (category != null) {
-            category = category.replace("|", "%7C"); // 특수 문자 처리
+        if (category_id != null) {
             result.put("campaigns",
-                    campaignService.findAllOfCategory(_sort[0], _sort[1], category));
+                    campaignService.findAllOfCategory(_sort[0], _sort[1], category_id));
         } else {
             result.put("campaigns", campaignService.findAll(_sort[0], _sort[1]));
         }
