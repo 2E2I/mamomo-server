1b59019abc6da14b457b295acb349a59cc9651b0
임유선 <1991167@hansung.ac.kr>
2022-03-17 21:17:20 +0900

    :lock: 토큰 security 설정

--- src/main/java/com/hsu/mamomo/webconfig/WebMvcConfig.java
+++ src/main/java/com/hsu/mamomo/webconfig/WebMvcConfig.java
@@ -1,4 +1,4 @@
-package com.hsu.mamomo.config;
+package com.hsu.mamomo.webconfig;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
---
+++ src/main/java/com/hsu/mamomo/webconfig/WebSecurityConfig.java
@@ -0,0 +1,66 @@
+package com.hsu.mamomo.webconfig;
+
+import com.hsu.mamomo.jwt.JwtAccessDeniedHandler;
+import com.hsu.mamomo.jwt.JwtAuthenticationEntryPoint;
+import com.hsu.mamomo.jwt.JwtSecurityConfig;
+import com.hsu.mamomo.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                // 토큰 방식을 사용하므로 csrf 설정을 disable
+                .csrf().disable()
+
+                // 예외처리
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+
+                // 세션 사용 X
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                // 접근 허용
+                .and()
+                .authorizeRequests()
+                .antMatchers("/api/campaigns").permitAll()
+                .antMatchers("/api/search/**").permitAll()
+                .antMatchers("/docs/api.html").permitAll()
+                .antMatchers("/api/signup").permitAll()
+                .anyRequest().authenticated()
+
+                .and()
+                .apply(new JwtSecurityConfig(jwtTokenProvider));
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+}
