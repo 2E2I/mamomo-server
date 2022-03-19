4fa6fb84f3ce59774e85b4bf343e50b16156aa1e
im-shung <seoyoung1113@gmail.com>
2022-03-20 00:35:19 +0900

    :white_check_mark: 회원가입 테스트 코드 작성

---
+++ src/test/java/com/hsu/mamomo/controller/UserControllerTest.java
@@ -0,0 +1,65 @@
+package com.hsu.mamomo.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.uuid.Generators;
+import com.hsu.mamomo.domain.User;
+import java.text.ParseException;
+import java.util.Date;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs
+class UserControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    private User user;
+
+    @BeforeEach
+    void setData() throws ParseException {
+        String id = Generators.randomBasedGenerator().generate().toString();
+
+        System.out.println("id = " + id);
+
+        String day = "20000101";
+        Date date = new java.text.SimpleDateFormat("yyyyMMdd").parse(day);
+
+        user = User.builder()
+                .id(id)
+                .email("user@email.com")
+                .password("user1234")
+                .nickname("user1")
+                .sex("M")
+                .birth(date)
+                .build();
+    }
+
+    @Test
+    @DisplayName("회원가입 테스트")
+    void signUpTest() throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        mockMvc.perform(post("/api/user/signup")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(user)))
+                .andExpect(status().isOk());
+    }
+
+
+}
