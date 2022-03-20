92e42eb8f722d6493ec0107adbbcbb1cf2aff95b
임유선 <1991167@hansung.ac.kr>
2022-03-20 21:44:59 +0900

    :sparkles: 로그인 컨트롤러, 로그인 관련 레포지토리 로직, User클래스 UserDetails implements-security 연동 (#15)

--- src/main/java/com/hsu/mamomo/controller/UserController.java
+++ src/main/java/com/hsu/mamomo/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.hsu.mamomo.controller;
 
 import com.hsu.mamomo.domain.User;
+import com.hsu.mamomo.dto.LoginDto;
 import com.hsu.mamomo.dto.UserDto;
 import com.hsu.mamomo.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/user")
 public class UserController {
 
     private final UserService userService;
@@ -23,8 +24,13 @@ public class UserController {
      * @param userDto
      * @return
      */
-    @PostMapping("/user/signup")
+    @PostMapping("/signup")
     public ResponseEntity<User> signUp(@RequestBody UserDto userDto) {
         return ResponseEntity.ok(userService.signUp(userDto));
+    }
+
+    @PostMapping("/signin")
+    public String signUp(@RequestBody LoginDto loginDto) {
+        return userService.signin(loginDto);
     }
 }
--- src/main/java/com/hsu/mamomo/repository/jpa/UserRepository.java
+++ src/main/java/com/hsu/mamomo/repository/jpa/UserRepository.java
@@ -1,6 +1,7 @@
 package com.hsu.mamomo.repository.jpa;
 
 import com.hsu.mamomo.domain.User;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,4 +9,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
 
+    Optional<User> findByEmail(String email);
 }
