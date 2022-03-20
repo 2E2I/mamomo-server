4f1867d372f08f2f53a6444b1847741a8fc6c111
임유선 <1991167@hansung.ac.kr>
2022-03-20 21:45:53 +0900

    :art: jwt provider~filter 방식 변경 (#15)

--- src/main/java/com/hsu/mamomo/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/hsu/mamomo/jwt/JwtAuthenticationFilter.java
@@ -6,6 +6,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -13,22 +14,17 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.filter.GenericFilterBean;
 
 @Slf4j
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends GenericFilterBean {
 
-    public static final String AUTHORIZATION_HEADER = "Authorization";
-
     private final JwtTokenProvider jwtTokenProvider;
-
-    public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider) {
-        this.jwtTokenProvider = jwtTokenProvider;
-    }
 
     // jwt 토큰의 인증정보를 현재 실행중인 스레드 SecurityContext에 저장
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
         // 헤더에서 JWT 를 받아옴
-        String token = resolveToken((HttpServletRequest) request);
+        String token = jwtTokenProvider.resolveToken((HttpServletRequest) request);
         String requestURI = ((HttpServletRequest) request).getRequestURI();
 
         // 유효한 토큰인지 확인
@@ -43,14 +39,5 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
             log.debug("유효한 JWT 토큰이 없습니다, uri: {}", requestURI);
         }
         chain.doFilter(request, response);
-    }
-
-    // HttpServletRequest 객체의 Header에서 token을 꺼냄
-    private String resolveToken(HttpServletRequest request) {
-        String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
-        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
-            return bearerToken.substring(7);
-        }
-        return null;
     }
 }
---
+++ src/main/java/com/hsu/mamomo/service/CustomUserDetailService.java
@@ -0,0 +1,21 @@
+package com.hsu.mamomo.service;
+
+import com.hsu.mamomo.repository.jpa.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException(String.format("사용자 %s 를 찾을 수 없습니다.", username)));
+    }
+}
