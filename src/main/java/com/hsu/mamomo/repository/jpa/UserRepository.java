49548db3a285906b250f79b53c7bee03b1053089
im-shung <seoyoung1113@gmail.com>
2022-03-21 12:13:48 +0900

    :sparkles: 회원가입 시 이메일/닉네임 존재 여부 검증 추가 (#52)

--- src/main/java/com/hsu/mamomo/repository/jpa/UserRepository.java
+++ src/main/java/com/hsu/mamomo/repository/jpa/UserRepository.java
@@ -10,4 +10,6 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository<User, UUID> {
 
     Optional<User> findByEmail(String email);
+
+    Optional<User> findByNickname(String nickname);
 }
