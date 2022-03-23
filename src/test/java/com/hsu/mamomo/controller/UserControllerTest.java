417a8f9deb86082b398f940bf277b1c7d8e3fee1
임유선 <1991167@hansung.ac.kr>
2022-03-23 22:23:35 +0900

    :white_check_mark: 로그인 api 문서화

--- src/test/java/com/hsu/mamomo/controller/UserControllerTest.java
+++ src/test/java/com/hsu/mamomo/controller/UserControllerTest.java
@@ -1,7 +1,21 @@
 package com.hsu.mamomo.controller;
 
+import static com.hsu.mamomo.document.ApiDocumentUtils.getDocumentRequest;
+import static com.hsu.mamomo.document.ApiDocumentUtils.getDocumentResponse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
+import static org.springframework.restdocs.headers.HeaderDocumentation.responseHeaders;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -11,18 +25,21 @@ import com.hsu.mamomo.domain.User;
 import com.hsu.mamomo.dto.TokenDto;
 import com.hsu.mamomo.jwt.JwtTokenProvider;
 import java.time.LocalDate;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -31,6 +48,7 @@ import org.springframework.test.web.servlet.MvcResult;
 @SpringBootTest
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs
+@TestMethodOrder(value = MethodOrderer.DisplayName.class)
 class UserControllerTest {
 
     @Autowired
@@ -41,6 +59,8 @@ class UserControllerTest {
 
     static private User user;
     static private Authority authority;
+
+    static private String jwtToken;
 
     @Autowired
     private JwtTokenProvider jwtTokenProvider;
@@ -57,12 +77,12 @@ class UserControllerTest {
                 .password("user1234")
                 .nickname("user1")
                 .sex("M")
-                .birth(LocalDate.of(2000,1,1))
+                .birth(LocalDate.of(2000, 1, 1))
                 .build();
     }
 
     @Test
-    @DisplayName("회원가입 테스트")
+    @DisplayName("1. 회원가입")
     void signUpTest() throws Exception {
 
         mockMvc.perform(post("/api/user/signup")
@@ -72,25 +92,82 @@ class UserControllerTest {
     }
 
     @Test
-    @DisplayName("로그인 테스트")
+    @DisplayName("2. 토큰 발급 (로그인)")
     void authenticationTest() throws Exception {
 
         Map<String, String> input = new HashMap<>();
-        input.put("email", "user@email.com");
-        input.put("password", "user1234");
+        input.put("email", user.getEmail());
+        input.put("password", user.getPassword());
 
-        MvcResult mvcResult = mockMvc.perform(post("/api/user/authenticate")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(input)))
+        MvcResult mvcResult = mockMvc
+                .perform(RestDocumentationRequestBuilders.post("/api/user/authenticate")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(input)))
                 .andExpect(status().isOk())
+
+                // 문서화
+                .andDo(document("authenticate-user",
+                        getDocumentRequest(),
+                        getDocumentResponse(),
+                        // 요청 필드 문서화
+                        requestFields(
+                                fieldWithPath("email").description("회원 이메일"),
+                                fieldWithPath("password").description("회원 비밀번호")
+                        ),
+                        responseHeaders(
+                                headerWithName(HttpHeaders.AUTHORIZATION)
+                                        .description("요청한 인증 정보가 유효하다면 JWT 토큰이 발급됩니다.")
+                        ),
+                        // 응답 바디 문서화
+                        responseFields(
+                                fieldWithPath("token")
+                                        .description("요청한 인증 정보가 유효하다면 JWT 토큰이 발급됩니다.")
+                        )))
+
                 .andReturn();
 
         // 응답 바디의 jwt 토큰 추출
         String responseBody = mvcResult.getResponse().getContentAsString();
         TokenDto tokenDto = objectMapper.readValue(responseBody, TokenDto.class);
+        jwtToken = tokenDto.getToken();
+        System.out.println("jwtToken = " + jwtToken);
 
         // 발급된 토큰이 유효한 jwt 토큰인지 확인
         assertTrue(jwtTokenProvider.validateToken(tokenDto.getToken()));
+    }
+
+    @DisplayName("3. 발급된 토큰으로 유저 정보 조회")
+    @Test
+    public void getUserInfoTest() throws Exception {
+        MvcResult mvcResult = mockMvc
+                .perform(RestDocumentationRequestBuilders.get("/api/user/{email}", user.getEmail())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + jwtToken))
+                .andExpect(status().isOk())
+
+                .andDo(document("get-user-info-with-jwtToken",
+                        pathParameters(
+                                parameterWithName("email").description("조회할 유저의 이메일")
+                        ),
+                        requestHeaders(
+                                headerWithName(HttpHeaders.AUTHORIZATION).description("api/user/authenticate 로 발급받은 조회할 유저의 토큰.\n"
+                                        + "토큰 문자열 앞에 'Bearer '(공백 한 개 포함) 을 붙입니다.")
+                        ),
+                        responseFields(
+                                fieldWithPath("id").description("유저 id"),
+                                fieldWithPath("email").description("유저 이메일"),
+                                fieldWithPath("password").description("null 리턴"),
+                                fieldWithPath("nickname").description("유저 별명"),
+                                fieldWithPath("sex").description("유저 성별"),
+                                fieldWithPath("birth").description("유저 생년월일"),
+                                fieldWithPath("profile").description("유저 프로필사진 url"),
+                                fieldWithPath("create_date").ignored(),
+                                fieldWithPath("modify_date").ignored(),
+                                fieldWithPath("authorities").ignored(),
+                                fieldWithPath("authorities.[].authorityName").ignored()
+                        )))
+                .andDo(print())
+                .andReturn();
     }
 
 }
