209356d326896e806e87db0708170d22a7de1cfe
임유선 <1991167@hansung.ac.kr>
2022-03-24 12:59:56 +0900

    :white_check_mark: 유저 정보 조회 assertion 추가

--- src/test/java/com/hsu/mamomo/controller/UserControllerTest.java
+++ src/test/java/com/hsu/mamomo/controller/UserControllerTest.java
@@ -2,6 +2,7 @@ package com.hsu.mamomo.controller;
 
 import static com.hsu.mamomo.document.ApiDocumentUtils.getDocumentRequest;
 import static com.hsu.mamomo.document.ApiDocumentUtils.getDocumentResponse;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
@@ -12,31 +13,35 @@ import static org.springframework.restdocs.payload.PayloadDocumentation.requestF
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
-
-import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.uuid.Generators;
-import com.hsu.mamomo.domain.Authority;
 import com.hsu.mamomo.domain.User;
 import com.hsu.mamomo.dto.TokenDto;
 import com.hsu.mamomo.jwt.JwtTokenProvider;
+import com.hsu.mamomo.repository.jpa.UserRepository;
+import com.hsu.mamomo.service.UserService;
 import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
+import javax.swing.text.html.Option;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
@@ -58,21 +63,18 @@ class UserControllerTest {
     ObjectMapper objectMapper;
 
     static private User user;
-    static private Authority authority;
 
     static private String jwtToken;
 
     @Autowired
     private JwtTokenProvider jwtTokenProvider;
+    @Mock
+    private static UserRepository userRepository;
 
     @BeforeAll
     static void setData() {
-        String id = Generators.randomBasedGenerator().generate().toString();
-
-        System.out.println("id = " + id);
 
         user = User.builder()
-                .id(id)
                 .email("user@email.com")
                 .password("user1234")
                 .nickname("user1")
@@ -150,8 +152,9 @@ class UserControllerTest {
                                 parameterWithName("email").description("조회할 유저의 이메일")
                         ),
                         requestHeaders(
-                                headerWithName(HttpHeaders.AUTHORIZATION).description("api/user/authenticate 로 발급받은 조회할 유저의 토큰.\n"
-                                        + "토큰 문자열 앞에 'Bearer '(공백 한 개 포함) 을 붙입니다.")
+                                headerWithName(HttpHeaders.AUTHORIZATION)
+                                        .description("api/user/authenticate 로 발급받은 조회할 유저의 토큰.\n"
+                                                + "토큰 문자열 앞에 'Bearer '(공백 한 개 포함) 을 붙입니다.")
                         ),
                         responseFields(
                                 fieldWithPath("id").description("유저 id"),
@@ -161,13 +164,32 @@ class UserControllerTest {
                                 fieldWithPath("sex").description("유저 성별"),
                                 fieldWithPath("birth").description("유저 생년월일"),
                                 fieldWithPath("profile").description("유저 프로필사진 url"),
-                                fieldWithPath("create_date").ignored(),
-                                fieldWithPath("modify_date").ignored(),
+                                fieldWithPath("create_date").description("회원가입 시간"),
+                                fieldWithPath("modify_date").description("마지막 회원 정보 수정 시간"),
                                 fieldWithPath("authorities").ignored(),
                                 fieldWithPath("authorities.[].authorityName").ignored()
                         )))
                 .andDo(print())
                 .andReturn();
+
+        // 응답 바디의 jwt 토큰 추출
+        String responseBody = mvcResult.getResponse().getContentAsString();
+        User responseUser = objectMapper.readValue(responseBody, User.class);
+
+        // 맞는 User를 반환했는지 검증
+        assertThat(responseUser)
+                .usingRecursiveComparison()
+                .ignoringFields("id", "password", "create_date", "modify_date", "authorities")
+                .ignoringFieldsOfTypes(User.class)
+                .isEqualTo(user);
+    }
+
+    // findByEmail Null 리턴함.
+    @AfterAll
+    static void deleteUser() {
+        Optional<User> selectUser = userRepository
+                .findByEmail(user.getEmail()); // Optional is Empty
+        selectUser.ifPresent(value -> userRepository.delete(value));
     }
 
 }
