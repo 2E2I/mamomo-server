6162ea86a83607ad8acecf71a4fdbd62904aab61
im-shung <seoyoung1113@gmail.com>
2022-03-26 00:25:50 +0900

    :sparkles: 회원가입 관련 Exception 처리 (#62)

--- src/main/java/com/hsu/mamomo/controller/UserController.java
+++ src/main/java/com/hsu/mamomo/controller/UserController.java
@@ -1,14 +1,25 @@
 package com.hsu.mamomo.controller;
 
+import static com.hsu.mamomo.controller.exception.ErrorCode.ABSENCE_OF_ESSENTIAL_FIELD;
+import static com.hsu.mamomo.controller.exception.ErrorCode.INVALID_FIELD;
+
+import com.hsu.mamomo.controller.exception.CustomException;
+import com.hsu.mamomo.controller.exception.ErrorResponse;
 import com.hsu.mamomo.domain.User;
 import com.hsu.mamomo.dto.LoginDto;
 import com.hsu.mamomo.dto.TokenDto;
 import com.hsu.mamomo.dto.UserDto;
 import com.hsu.mamomo.service.UserService;
+import java.util.Objects;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,15 +36,16 @@ public class UserController {
     private final UserService userService;
 
     /**
-     *
      * 회원가입
+     *
      * @param userDto
      * @return
      */
     @PostMapping("/signup")
-    public ResponseEntity<User> signUp(@RequestBody UserDto userDto) {
-        return ResponseEntity.ok(userService.signUp(userDto));
+    public ResponseEntity<UserDto> signUp(@RequestBody @Valid UserDto userDto) {
+        return userService.signUp(userDto);
     }
+
 
     @PostMapping("/authenticate")
     public ResponseEntity<TokenDto> signUp(@RequestBody LoginDto loginDto) {
@@ -46,4 +58,30 @@ public class UserController {
         log.info("{} 의 정보를 찾습니다 ..", email);
         return userService.getUserInfo().get();
     }
+
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationException(
+            MethodArgumentNotValidException exception) {
+        ObjectError objectError = Objects.requireNonNull(
+                exception.getBindingResult().getAllErrors().stream().findFirst()
+                        .orElse(null));
+        log.error("handleValidationException throw MethodArgumentNotValidException : {}",
+                INVALID_FIELD);
+        return ErrorResponse.toResponseEntity(INVALID_FIELD, objectError.getDefaultMessage());
+    }
+
+    @ExceptionHandler(value = HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleNotReadableException() {
+        log.error("handleNotReadableException throw Exception : {}", ABSENCE_OF_ESSENTIAL_FIELD);
+        return ErrorResponse.toResponseEntity(ABSENCE_OF_ESSENTIAL_FIELD);
+    }
+
+    @ExceptionHandler(value = CustomException.class)
+    public ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
+        log.error("handleCustomException throw CustomException : {}", e.getErrorCode());
+        return ErrorResponse.toResponseEntity(e.getErrorCode());
+    }
+
+
 }
