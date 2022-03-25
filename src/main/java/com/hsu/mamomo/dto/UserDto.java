99662641124178f0c5dc94daccef3c01ea8119c6
im-shung <seoyoung1113@gmail.com>
2022-03-26 00:15:15 +0900

    :art: UserDto 필드들 검증 형식 추가 및 birth 타입 변경 - birth 필드에 @Pattern 어노테이션 추가하기 위해 타입 변경 - nickname과 password의 정규식 표현은 나중에 프론트에 맞춰서 변경할 것 (#52)

--- src/main/java/com/hsu/mamomo/dto/UserDto.java
+++ src/main/java/com/hsu/mamomo/dto/UserDto.java
@@ -1,10 +1,15 @@
 package com.hsu.mamomo.dto;
 
-import java.time.LocalDate;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Builder
 @Getter
@@ -12,15 +17,23 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UserDto {
 
+    @NotBlank(message = "이메일은 필수 입력값입니다.")
+    @Email(message = "이메일 형식에 맞지 않습니다.")
     private String email;
 
+    @NotBlank(message = "비밀번호는 필수 입력값입니다.")
     private String password;
 
+    @NotBlank(message = "닉네임은 필수 입력값입니다.")
+    @Pattern(regexp = "^[a-zA-Z|가-힣]+[0-9]*$",message = "닉네임은 한글, 영문, 숫자만 가능합니다.")
+    @Length(min = 2, max = 8,message = "닉네임은 길이가 2에서 8 사이여야 합니다.")
     private String nickname;
 
-    private String profile;
-
+    @Pattern(regexp = "^[MF]$",message = "성별은 M 또는 F 중 하나여야 합니다.")
     private String sex;
 
-    private LocalDate birth;
+    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Pattern(regexp = "^((19|20)\\d{2}-(0[1-9]|1[0-2])-(0[1-9]|[12]\\d|3[01]))$",message = "날짜 형식은 [yyyy-mm-dd]입니다.")
+    private String birth;
 }
