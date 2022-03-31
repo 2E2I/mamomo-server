ee1c687acb4eed1e459b398cda466866ed3b0ce7
im-shung <seoyoung1113@gmail.com>
2022-04-01 00:47:49 +0900

    :sparkles: 관심 기부 분야 DB 저장 로직 - List<FavTopic>에 Topic과 User 저장 - User에 List<FavTopic> 저장 - 즉, User와 FavTopic에 양방향으로 저장함! - FavTopic에는 Topic을 단방향으로 저장함! (close #64)

---
+++ src/main/java/com/hsu/mamomo/repository/jpa/FavTopicRepository.java
@@ -0,0 +1,12 @@
+package com.hsu.mamomo.repository.jpa;
+
+import com.hsu.mamomo.domain.FavTopic;
+import com.hsu.mamomo.domain.User;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FavTopicRepository extends JpaRepository<FavTopic, Long> {
+
+    Optional<List<FavTopic>> findFavTopicByUser(User user);
+}
