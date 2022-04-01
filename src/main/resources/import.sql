48d6d8812ddbe2a53e26bd0caf699e33f9571472
임유선 <1991167@hansung.ac.kr>
2022-04-01 18:33:10 +0900

    :card_file_box: id uuid로 변경

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -13,4 +13,4 @@ INSERT INTO topic (topic_id, topic_name) value (8,'우리사회');
 INSERT INTO topic (topic_id, topic_name) value (9,'동물');
 INSERT INTO topic (topic_id, topic_name) value (10,'환경');
 
-INSERT INTO user (id, birth, email, nickname, password, sex) value ('testId','2000-01-01','test@email.com','testNickName','{bcrypt}$2a$10$GXbyN.DHQXq1gWKKsVu7DeQ6Gc4kE8QeFb..RpOHHYw00sRQRkNha','M');
+INSERT INTO user (id, birth, email, nickname, password, sex) value ('550e8400-e29b-41d4-a716-446655440000','2000-01-01','test@email.com','testNickName','{bcrypt}$2a$10$GXbyN.DHQXq1gWKKsVu7DeQ6Gc4kE8QeFb..RpOHHYw00sRQRkNha','M');
