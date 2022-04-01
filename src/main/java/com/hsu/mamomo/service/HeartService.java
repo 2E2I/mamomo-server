736130e4bc374f451a8aea59967384550629bbc8
임유선 <1991167@hansung.ac.kr>
2022-04-01 18:33:53 +0900

    :zap: 좋아요 중복 방지 로직 추가 (#72)

--- src/main/java/com/hsu/mamomo/service/HeartService.java
+++ src/main/java/com/hsu/mamomo/service/HeartService.java
@@ -1,10 +1,18 @@
 package com.hsu.mamomo.service;
 
+import static com.hsu.mamomo.controller.exception.ErrorCode.ALREADY_HEARTED;
+import static com.hsu.mamomo.controller.exception.ErrorCode.HEART_NOT_FOUND;
+import static com.hsu.mamomo.controller.exception.ErrorCode.MEMBER_NOT_FOUND;
+
+import com.hsu.mamomo.controller.exception.CustomException;
+import com.hsu.mamomo.controller.exception.ErrorCode;
 import com.hsu.mamomo.domain.Heart;
 import com.hsu.mamomo.domain.User;
 import com.hsu.mamomo.dto.HeartDto;
 import com.hsu.mamomo.repository.jpa.HeartRepository;
 import com.hsu.mamomo.repository.jpa.UserRepository;
+import java.util.Optional;
+import javax.swing.text.html.Option;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -18,6 +26,11 @@ public class HeartService {
     private final UserRepository userRepository;
 
     public void heart(HeartDto heartDto) {
+
+        // 이미 좋아요 된 캠페인일 경우 409 에러
+        if (findHeartWithUserAndCampaignId(heartDto).isPresent())
+            throw new CustomException(ALREADY_HEARTED);
+
         Heart heart = Heart.builder()
                 .campaignId(heartDto.getCampaignId())
                 .user(userRepository.findUserById(heartDto.getUserId()).get())
@@ -26,9 +39,20 @@ public class HeartService {
     }
 
     public void unHeart(HeartDto heartDto) {
-        User user = userRepository.findUserById(heartDto.getUserId()).get();
-        Heart heart = heartRepository.findHeartByUserAndCampaignId(user, heartDto.getCampaignId()).get();
-        heartRepository.delete(heart);
+        Optional<Heart> heartOpt = findHeartWithUserAndCampaignId(heartDto);
+
+        if (heartOpt.isEmpty())
+            throw new CustomException(HEART_NOT_FOUND);
+
+        heartRepository.delete(heartOpt.get());
+    }
+
+    public Optional<Heart> findHeartWithUserAndCampaignId(HeartDto heartDto) {
+        Optional<User> userOpt = userRepository.findUserById(heartDto.getUserId());
+        if (userOpt.isEmpty())
+            throw new CustomException(MEMBER_NOT_FOUND);
+
+        return heartRepository.findHeartByUserAndCampaignId(userOpt.get(), heartDto.getCampaignId());
     }
 
 }
