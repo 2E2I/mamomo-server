fcca51cf7bf498a30881400f79f9b1062c12607b
임유선 <1991167@hansung.ac.kr>
2022-04-06 13:14:12 +0900

    :truck: Heartcontroller -> HeartController

--- src/main/java/com/hsu/mamomo/controller/HeartController.java
+++ src/main/java/com/hsu/mamomo/controller/HeartController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/heart")
-public class Heartcontroller {
+public class HeartController {
 
     private final HeartService heartService;
 
