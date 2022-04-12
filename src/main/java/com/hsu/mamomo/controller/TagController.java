cf28496640b2c3aa9ac30d5f357670d760543fc6
임유선 <1991167@hansung.ac.kr>
2022-04-12 20:59:46 +0900

    :sparkles: 태그별 조회 api, 인기태그 범위 설정 api 변경 (#93, #94)
    - ElasticTagFactory 추가하여 태그 관련 API Elasticsearch 쿼리 동작하도록
      설정
    - 태그별 조회 api 추가
    - 인기태그 조회는 범위를 설정하도록 변경

--- src/main/java/com/hsu/mamomo/controller/TagController.java
+++ src/main/java/com/hsu/mamomo/controller/TagController.java
@@ -1,24 +1,43 @@
 package com.hsu.mamomo.controller;
 
+import com.hsu.mamomo.dto.CampaignDto;
 import com.hsu.mamomo.dto.TagDto;
+import com.hsu.mamomo.service.CampaignService;
 import com.hsu.mamomo.service.TagService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RequestMapping("/api/search")
+@RequestMapping("/api")
 @RestController
 public class TagController {
 
-    private final TagService campaignSearchService;
+    private final TagService tagService;
+    private final CampaignService campaignService;
 
-    @GetMapping
-    public ResponseEntity<TagDto> searchTag() {
+    @GetMapping("/search")
+    public ResponseEntity<TagDto> getTags(
+            @RequestParam(value = "from", defaultValue = "0") Integer from,
+            @RequestParam(value = "to", defaultValue = "1000") Integer to
+    ) {
         return ResponseEntity.ok()
-                .body(new TagDto(campaignSearchService.findTop10Tags()));
+                .body(new TagDto(tagService.getRangeTags(from, to)));
+    }
+
+    @GetMapping("/tag/{tagName}")
+    public CampaignDto searchByTag(
+            @PathVariable(value = "tagName") String tagName,
+            @PageableDefault(size = 20, sort = "start_date", direction = Direction.DESC) Pageable pageable
+    ) {
+        return campaignService.findAllOfTag(tagName, pageable);
     }
 
 }
---
+++ src/main/java/com/hsu/mamomo/service/factory/ElasticTagFactory.java
@@ -0,0 +1,70 @@
+package com.hsu.mamomo.service.factory;
+
+import static java.util.stream.Collectors.toList;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.aggregations.Aggregation;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.bucket.MultiBucketsAggregation;
+import org.elasticsearch.search.aggregations.bucket.terms.ParsedStringTerms;
+import org.elasticsearch.search.aggregations.bucket.terms.TermsAggregationBuilder;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ElasticTagFactory extends ElasticSortFactory {
+
+    private final RestHighLevelClient elasticsearchClient;
+
+    public ElasticTagFactory(
+            ElasticsearchOperations elasticsearchOperations,
+            RestHighLevelClient elasticsearchClient) {
+        super(elasticsearchOperations);
+        this.elasticsearchClient = elasticsearchClient;
+    }
+
+    @Override
+    public NativeSearchQuery createQuery(String keyword, Pageable pageable) {
+        return new NativeSearchQueryBuilder()
+                .withQuery(QueryBuilders.matchQuery("tags.keyword", keyword))
+                .withSorts(createSortBuilder(pageable))
+                .withPageable(pageable)
+                .build();
+    }
+
+    public List<String> getTags(int from, int to) throws IOException {
+        TermsAggregationBuilder aggregation =
+                AggregationBuilders.terms("tags")
+                        .field("tags.keyword")
+                        .size(to);
+
+        SearchSourceBuilder builder = new SearchSourceBuilder().aggregation(aggregation);
+        SearchRequest searchRequest = new SearchRequest("campaigns").source(builder);
+
+        SearchResponse response = elasticsearchClient.search(searchRequest,
+                RequestOptions.DEFAULT);
+
+        Map<String, Aggregation> results = response.getAggregations()
+                .asMap();
+
+        ParsedStringTerms topTags = (ParsedStringTerms) results.get("tags");
+
+        return topTags.getBuckets()
+                .stream()
+                .map(MultiBucketsAggregation.Bucket::getKeyAsString)
+                .sorted()
+                .collect(toList())
+                .subList(from, to);
+    }
+}
