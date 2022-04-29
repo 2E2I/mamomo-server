098b3f0cf342e45ada54f1de1b50f91adbc79d0d
im-shung <seoyoung1113@gmail.com>
2022-04-29 11:39:23 +0900

    :sparkles: client로부터 텍스트 받는 TextDto 생성

---
+++ src/main/java/com/hsu/mamomo/dto/TextDto.java
@@ -0,0 +1,17 @@
+package com.hsu.mamomo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TextDto {
+
+    private String text;
+}
