47bb90736cbe0cf140383059a1d306d76036223a
im-shung <seoyoung1113@gmail.com>
2022-04-29 11:39:39 +0900

    :sparkles: flask로부터 텍스트마이닝 결과 받는 TextMiningResultDto 생성

---
+++ src/main/java/com/hsu/mamomo/dto/TextMiningResultDto.java
@@ -0,0 +1,18 @@
+package com.hsu.mamomo.dto;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TextMiningResultDto {
+
+    private List<String> result;
+}
