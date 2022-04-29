89b6253006ed8166b1eb9eb10d296cd3ad44a65f
im-shung <seoyoung1113@gmail.com>
2022-04-29 11:42:29 +0900

    :sparkles: Flask로 텍스트 전송하는 API 구현 requestTextMining() : client의 POST 요청으로 text를 TextDto에 담아 flask에게 보낸다 (#107)

---
+++ src/main/java/com/hsu/mamomo/service/TextMiningService.java
@@ -0,0 +1,54 @@
+package com.hsu.mamomo.service;
+
+import com.hsu.mamomo.domain.Campaign;
+import com.hsu.mamomo.dto.CampaignDto;
+import com.hsu.mamomo.dto.TextDto;
+import com.hsu.mamomo.dto.TextMiningResultDto;
+import com.hsu.mamomo.service.factory.ElasticTextMiningFactory;
+import java.net.URI;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class TextMiningService {
+
+    @Value("http://127.0.0.1:5000")
+    private String flaskApiUrl;
+
+    private final ElasticTextMiningFactory textMiningFactory;
+
+    public CampaignDto requestTextMining(Pageable pageable, TextDto textDto) {
+        URI uri = UriComponentsBuilder
+                .fromUriString(flaskApiUrl)
+                .path("/")
+                .build()
+                .toUri();
+
+        RequestEntity<TextDto> requestEntity = RequestEntity
+                .post(uri)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(textDto);
+
+        RestTemplate restTemplate = new RestTemplate();
+        TextMiningResultDto textMiningResultDto = restTemplate.exchange(requestEntity, TextMiningResultDto.class).getBody();
+        return getCampaignsByTextMining(textMiningResultDto, pageable);
+    }
+
+    public CampaignDto getCampaignsByTextMining(TextMiningResultDto textMiningResultDto, Pageable pageable) {
+        return new CampaignDto(findCampaignsByTextMining(textMiningResultDto, pageable));
+    }
+
+    public Page<Campaign> findCampaignsByTextMining(TextMiningResultDto textMiningResultDto, Pageable pageable) {
+        return textMiningFactory.getCampaignSearchList(textMiningFactory.createQuery(textMiningResultDto,pageable));
+    }
+}
