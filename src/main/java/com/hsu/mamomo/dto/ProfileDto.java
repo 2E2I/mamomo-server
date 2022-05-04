47b1e9e961d74fd3a0b8098e9a2f87a9ef6689cf
임유선 <1991167@hansung.ac.kr>
2022-05-05 01:55:25 +0900

    :sparkles: 프로필 수정 api 추가 (#115)

---
+++ src/main/java/com/hsu/mamomo/dto/ProfileDto.java
@@ -0,0 +1,24 @@
+package com.hsu.mamomo.dto;
+
+import com.hsu.mamomo.domain.FavTopic;
+import java.time.LocalDate;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProfileDto {
+
+    private String profile;
+    private String nickname;
+    private String sex;
+    private LocalDate birth;
+    private List<FavTopic> favTopics;
+}
