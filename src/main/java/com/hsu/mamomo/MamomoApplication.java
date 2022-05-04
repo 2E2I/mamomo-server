29dc661c0a5caaccdd2088f7b0098b3bd900badd
임유선 <1991167@hansung.ac.kr>
2022-05-05 02:21:38 +0900

    :wrench: timezone asia/seoul로 변경, 회원가입시 시간 적용 됨 (#115)

--- src/main/java/com/hsu/mamomo/MamomoApplication.java
+++ src/main/java/com/hsu/mamomo/MamomoApplication.java
@@ -1,5 +1,7 @@
 package com.hsu.mamomo;
 
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -8,6 +10,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableJpaRepositories("com.hsu.mamomo.repository.jpa")
 public class MamomoApplication {
+
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MamomoApplication.class, args);
